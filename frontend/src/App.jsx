4bd3a48cf01bf37d13d04317173ca27269ebd059
sofiene chouchene <chouchene.sofiene@outlook.com>
2023-04-03 01:43:03 +0100

    setup react app with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline ">sofiene</h1>
+      
+    </div>
+  );
+}
+
+export default App;
